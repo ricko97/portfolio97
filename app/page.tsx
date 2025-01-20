664739d8b3aaed51b0cf3087f15688044bceebcc
Rick Lionel Onana <oricklionel@gmail.com>
2025-01-20 02:42:35 -0500

    Fix Homepage image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Button, Card, CardFooter, Divider, Image } from "@heroui/react";
+import { Button, Card, CardFooter, Divider, Image, Link } from "@heroui/react";
 import clsx from "clsx";
 
 import { title, subtitle } from "@/components/primitives";
@@ -12,7 +12,7 @@ import NextLink from "next/link";
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <section className="flex max-sm:flex-col flex-row justify-between max-sm:gap-12 lg:gap-32 px-4">
+      <section className="flex max-sm:flex-col flex-row items-center justify-between max-sm:gap-8 md:gap-16 lg:gap-16 px-4">
         <div className="flex flex-col">
           <span className={title({ color: "blue" })}>{siteConfig.name}</span>
           <div className={subtitle({ class: "mt-4" })}>{siteConfig.intro}</div>
@@ -23,15 +23,22 @@ export default function Home() {
             fullWidth={false}
             variant="shadow"
           >
-            <NextLink href={"/education"}>Let&#39;s get started</NextLink>
+            <NextLink href={"/education"}>Lets get started</NextLink>
           </Button>
         </div>
         <Image
           alt={"Rick Lionel Onana"}
-          className={"rounded-full shadow-inner-xl"}
-          height={250}
+          className="rounded-full shadow-inner-xl max-sm:hidden"
+          height="auto"
+          width={900}
           src={"/img/me.jpeg"}
-          width={750}
+        />
+        <Image
+          alt={"Rick Lionel Onana"}
+          className="rounded-full shadow-inner-xl lg:hidden md:hidden"
+          height="auto"
+          src={"/img/me.jpeg"}
+          width={300}
         />
       </section>
       <div
@@ -60,7 +67,7 @@ export default function Home() {
                 <Image
                   alt={tech.name}
                   className="object-cover"
-                  height={100}
+                  height="auto"
                   src={tech.image}
                   width={100}
                 />
